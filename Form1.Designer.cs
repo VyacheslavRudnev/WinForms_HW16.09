5f9972cbf3691f91c9e4b0d1e5c0be0ad28ecb1c
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-16 17:47:32 +0300

    HW task 02 done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,74 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(128, 255, 128);
-            button1.FlatAppearance.BorderColor = Color.WhiteSmoke;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 192, 0);
-            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 255, 128);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(153, 238);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(187, 74);
-            button1.TabIndex = 0;
-            button1.Text = "ТАК";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(255, 128, 128);
-            button2.FlatAppearance.BorderColor = Color.White;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatAppearance.MouseDownBackColor = Color.Red;
-            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 128, 128);
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(457, 238);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Name = "button2";
-            button2.Size = new Size(187, 72);
-            button2.TabIndex = 1;
-            button2.Text = "НІ";
-            button2.UseVisualStyleBackColor = false;
-            button2.MouseMove += button2_MouseMove;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.SeaShell;
-            label1.FlatStyle = FlatStyle.Flat;
-            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.ImageAlign = ContentAlignment.BottomCenter;
-            label1.Location = new Point(72, 96);
+            label1.Font = new Font("Calibri", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(674, 44);
-            label1.TabIndex = 2;
-            label1.Text = "Чи задоволені ви розміром своєї зарплати?";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
-            label1.UseCompatibleTextRendering = true;
+            label1.Size = new Size(147, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Завдання №2";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(512, 140);
+            label2.TabIndex = 1;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = Color.LightSkyBlue;
+            button1.CausesValidation = false;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Calibri", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Navy;
+            button1.Location = new Point(208, 222);
+            button1.Name = "button1";
+            button1.Size = new Size(332, 40);
+            button1.TabIndex = 2;
+            button1.Text = "ГРА \"ВГАДАЙ ЧИСЛО\"\r\n";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 224, 192);
+            BackColor = Color.WhiteSmoke;
             ClientSize = new Size(800, 451);
-            Controls.Add(label1);
-            Controls.Add(button2);
             Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             SizeGripStyle = SizeGripStyle.Show;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             ResumeLayout(false);
             PerformLayout();
@@ -108,8 +93,8 @@
 
         #endregion
 
+        private Label label1;
+        private Label label2;
         private Button button1;
-        private Button button2;
-        public Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,41 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void button2_MouseMove(object sender, MouseEventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            button2.Top += e.Y;
-            button2.Left -= e.X;
-            if (button2.Top < -10 || button2.Top > 100)
-                button2.Top = 60;
-            if (button2.Left < -80 || button2.Left > 250)
-                button2.Left = 120;
+            string message1 = "Загадай число від 1 до 2000";
+            string title = "Вгадай число";
+            MessageBox.Show(message1, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            int min = 1;
+            int max = 2000;
+            int number = 0;
+            int count = 0;
+            while (true)
+            {
+                count++;
+                number = (min + max) / 2;
+                string message2 = $"Ти загадав число {number} вірно?";
+                DialogResult result = MessageBox.Show(message2, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    string message3 = $"Я вгадав число {number} за {count} спроб";
+                    MessageBox.Show(message3, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                }
+                else
+                {
+                    string message3 = $"Число {number} більше загаданого?";
+                    DialogResult result2 = MessageBox.Show(message3, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result2 == DialogResult.Yes)
+                    {
+                        max = number;
+                    }
+                    else
+                    {
+                        min = number;
+                    }
+                }
+            }
         }
     }
 }
