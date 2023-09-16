1a41168e76ef21840fae9096c1d2b463c8bec1cb
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-16 16:04:13 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.FlatAppearance.BorderColor = Color.WhiteSmoke;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 192, 0);
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 255, 128);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(153, 238);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(187, 74);
+            button1.TabIndex = 0;
+            button1.Text = "ТАК";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 128);
+            button2.FlatAppearance.BorderColor = Color.White;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.Red;
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 128, 128);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(457, 238);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(187, 72);
+            button2.TabIndex = 1;
+            button2.Text = "НІ";
+            button2.UseVisualStyleBackColor = false;
+            button2.MouseMove += button2_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.SeaShell;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.ImageAlign = ContentAlignment.BottomCenter;
+            label1.Location = new Point(72, 96);
+            label1.Name = "label1";
+            label1.Size = new Size(674, 44);
+            label1.TabIndex = 2;
+            label1.Text = "Чи задоволені ви розміром своєї зарплати?";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(800, 451);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Show;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ми й не сумнівалися, що Ви саме так вважаєте!");
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        public Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            button2.Top += e.Y;
+            button2.Left -= e.X;
+            if (button2.Top < -10 || button2.Top > 100)
+                button2.Top = 60;
+            if (button2.Left < -80 || button2.Left > 250)
+                button2.Left = 120;
+        }
+    }
+}
