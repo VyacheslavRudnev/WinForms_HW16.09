de6c706f09a910b16f7692fc7fdb2f30b46d62db
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-22 17:54:51 +0300

    HW task 03 done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,88 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             SuspendLayout();
             // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(128, 255, 128);
-            button1.FlatAppearance.BorderColor = Color.WhiteSmoke;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 192, 0);
-            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 255, 128);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(153, 238);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(187, 74);
-            button1.TabIndex = 0;
-            button1.Text = "ТАК";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(255, 128, 128);
-            button2.FlatAppearance.BorderColor = Color.White;
-            button2.FlatAppearance.BorderSize = 0;
-            button2.FlatAppearance.MouseDownBackColor = Color.Red;
-            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 128, 128);
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Font = new Font("Calibri", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(457, 238);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Name = "button2";
-            button2.Size = new Size(187, 72);
-            button2.TabIndex = 1;
-            button2.Text = "НІ";
-            button2.UseVisualStyleBackColor = false;
-            button2.MouseMove += button2_MouseMove;
-            // 
             // label1
             // 
+            label1.AccessibleRole = AccessibleRole.None;
             label1.AutoSize = true;
-            label1.BackColor = Color.SeaShell;
+            label1.Enabled = false;
             label1.FlatStyle = FlatStyle.Flat;
-            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = SystemColors.ActiveCaptionText;
-            label1.ImageAlign = ContentAlignment.BottomCenter;
-            label1.Location = new Point(72, 96);
+            label1.Location = new Point(137, 9);
             label1.Name = "label1";
-            label1.Size = new Size(674, 44);
-            label1.TabIndex = 2;
-            label1.Text = "Чи задоволені ви розміром своєї зарплати?";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
-            label1.UseCompatibleTextRendering = true;
+            label1.Size = new Size(542, 160);
+            label1.TabIndex = 0;
+            label1.Text = resources.GetString("label1.Text");
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 224, 192);
+            BackColor = Color.FromArgb(224, 224, 224);
             ClientSize = new Size(800, 451);
             Controls.Add(label1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
             SizeGripStyle = SizeGripStyle.Show;
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MyForm";
+            MouseClick += Form1_MouseClick;
             ResumeLayout(false);
             PerformLayout();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Ми й не сумнівалися, що Ви саме так вважаєте!");
-        }
-
         #endregion
 
-        private Button button1;
-        private Button button2;
-        public Label label1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,52 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void button2_MouseMove(object sender, MouseEventArgs e)
+        public event MouseEventHandler MoveMous;
+        public delegate void MouseEventHandler(Object sender, MouseEventArgs e);
+        private string CoordinatesToString(MouseEventArgs e)
         {
-            button2.Top += e.Y;
-            button2.Left -= e.X;
-            if (button2.Top < -10 || button2.Top > 100)
-                button2.Top = 60;
-            if (button2.Left < -80 || button2.Left > 250)
-                button2.Left = 120;
+            return "Координати миші: х=" + e.X.ToString() + "; y=" + e.Y.ToString();
+        }
+        private void Form1_MoveMous(object sender, MouseEventArgs e)
+        {
+            Text = CoordinatesToString(e);
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            String message = "";
+            if (e.Button == MouseButtons.Right)
+            {
+                message = "Ви натиснули праву кнопку миші." +
+                    "\nШирина робочої області = " + Width + "," +
+                    "\nВисота робочої області= " + Height;
+
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                if (Control.ModifierKeys != Keys.Control)
+                {
+                    message = "Ви натиснули ліву кнопку миші.";
+                    if (e.X > 15 && e.X < 782 && e.Y > 15 && e.Y < 434)
+                    {
+                        MessageBox.Show("Вказівник всередені прямокутника");
+                    }
+                    else if (e.X < 10 || e.X > 787 || e.Y < 10 || e.Y > 439)
+                    {
+                        MessageBox.Show("Вказівник зовні прямoкутника");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Вказівник на межі прямокутника");
+                    }
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            message += "\n" + CoordinatesToString(e);
+            String caption = "Клик мыши";
+            MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
